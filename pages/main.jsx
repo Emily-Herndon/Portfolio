aab2797aec971724f4b841ea779de83ea60ff50f
Emily-Herndon <emilyvherndon@gmail.com>
2022-08-09 10:04:13 -0700

    resume and main tweaks

--- pages/main.jsx
+++ pages/main.jsx
@@ -12,9 +12,9 @@ export default function Main() {
                         width={200}
                         height={200}
                     />
-                    <h1>{`Hi, I'm`} <span className="text-cyan-300">Emily</span></h1>
-                    <h2>I&apos;m <span id="flip">a Software Engineer</span></h2>
-                    <p className="py-4 m-auto max-w-[70%] text-black text-xl">I have an intellectual curiosity, love for learning, and the ability to work both independently and collaboratively. I am passionate about developing accessible products to better {`people's`} lives and enjoy solving complex puzzles. I believe software engineering is a conduit to create a positive and meaningful impact. I strive to make products with accessible, unique interfaces and adaptability for the ever-changing future.</p>
+                    <h1 className="text-white">{`Hi, I'm`} <span className="text-cyan-300">Emily</span></h1>
+                    <h2 className="text-white">I&apos;m <span id="flip">a Software Engineer</span></h2>
+                    <p className="py-4 m-auto max-w-[70%] text-xl text-white">I have an intellectual curiosity, love for learning, and the ability to work both independently and collaboratively. I am passionate about developing accessible products to better {`people's`} lives and enjoy solving complex puzzles. I believe software engineering is a conduit to create a positive and meaningful impact. I strive to make products with accessible, unique interfaces and adaptability for the ever-changing future.</p>
                 </div>
             </div>
         </div>
